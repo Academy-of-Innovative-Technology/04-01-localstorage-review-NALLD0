52de55d6110e533dbebc6c07593d4f6db9a3b339
NALLD0 <naldon@nycstudents.net>
2026-03-16 01:06:32 -0400

    completed

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-var key = "";
+var key = "myDataKey";
 
 
 // SAVE
 document.querySelector(".save").onclick = function() {
+
+    var value = document.getElementById("saveValue").value;
+
+    localStorage.setItem(key, value);
+
+    document.getElementById("saveValue").value = "";
+    alert("Data saved!");
 
 };
 
 
 // LOAD
 document.querySelector(".load").onclick = function() {
+    var storedValue = localStorage.getItem(key);
 
+    document.getElementById("loadValue").value = storedValue ? storedValue : "";
 };
 
 // LOAD
 document.querySelector(".update").onclick = function() {
 
+    var newValue = document.getElementById("updateValue").value;
+
+    localStorage.setItem(key, newValue );
+
+    document.getElementById("updateValue").value = "";
+
+    alert("Data updated!");
+
 };
 
 // DELETE
-document.querySelector(".delete").onclick = function() {
+function deleteData () {
 
-};
+    var deleteKeyInput = document.getElementById("deleteKey").value;
+
+    localStorage.removeItem(deleteKeyInput);
+
+    document.getElementById("deleteKey").value = "";
+    
+    alert("Data deleted!");
+}
